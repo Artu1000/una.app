cd8df25773188abe9942e6f7442d8e4e510f9fcf
Arthur LORENT <artu1000>
2015-09-16 14:12:15 +0200

    gitignore updates & responsive fixes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,21 +2,24 @@
 
 @section('content')
 
-    <div class="container error text-center">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="error_container row">
-                    <div class="error_image">
+    <div id="content" class="error row fill">
+        <div class="container fill">
+
+            <div class="display-table fill">
+
+                <div class="v-center text-center table-cell">
+
+                    <div class="image">
                         <img width="300" height="280" src="{{ env('SITE_LOGO') }}" alt="Logo {{ env('SITE_NAME') }}">
                     </div>
-                    <div class="error_title">
+                    <div class="title">
                         <h1>Oups !</h1>
                         <h2>Erreur 404 / Page introuvable</h2>
                     </div>
-                    <div class="error_details">
+                    <div class="details">
                         <p class="quote">La page demandée n'existe pas.</p>
                     </div>
-                    <div class="error_actions">
+                    <div class="actions">
                         <a href="{{ url() }}" title="Revenir à l'accueil de {{ env('SITE_NAME') }}" class="btn btn-primary btn-lg">
                             <i class="fa fa-home"></i> Revenir à l'accueil
                         </a>
