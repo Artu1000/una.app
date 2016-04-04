e1648452d55d76983d684cb4dfdf97047209438b
Arthur LORENT <arthur@acid.fr>
2016-04-04 16:10:15 +0200

    app provider updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,17 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Approached\LaravelImageOptimizer\ServiceProvider as LaravelImageOptimizerServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Cartalyst\Sentinel\Laravel\SentinelServiceProvider;
+use Cviebrock\ImageValidator\ImageValidatorServiceProvider;
+use DaveJamesMiller\Breadcrumbs\ServiceProvider as BreadcrumbsServiceProvider;
+use Dimsav\Translatable\TranslatableServiceProvider;
+use Intervention\Image\ImageServiceProvider;
+use Laracasts\Generators\GeneratorsServiceProvider;
+use Laracasts\Utilities\JavaScript\JavaScriptServiceProvider;
+use Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider;
+use Propaganistas\LaravelPhone\LaravelPhoneServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,46 +35,46 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment() == 'local') {
             // database generator service
-            $this->app->register(\Laracasts\Generators\GeneratorsServiceProvider::class);
+            $this->app->register(GeneratorsServiceProvider::class);
 
             // ide helper
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->register(IdeHelperServiceProvider::class);
         }
 
         // laracast javascript service
         // https://github.com/laracasts/PHP-Vars-To-Js-Transformer
-        $this->app->register(\Laracasts\Utilities\JavaScript\JavascriptServiceProvider::class);
+        $this->app->register(JavaScriptServiceProvider::class);
 
         // image optimizer
         // https://github.com/approached/laravel-image-optimizer
-        $this->app->register(\Approached\LaravelImageOptimizer\ServiceProvider::class);
+        $this->app->register(LaravelImageOptimizerServiceProvider::class);
 
         // sentinel
         // https://cartalyst.com/manual/sentinel/2.0
-        $this->app->register(\Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class);
+        $this->app->register(SentinelServiceProvider::class);
 
         // intervention image management library
         // http://image.intervention.io/getting_started/installation#laravel
-        $this->app->register(\Intervention\Image\ImageServiceProvider::class);
+        $this->app->register(ImageServiceProvider::class);
 
         // laravel phone validator
         // https://github.com/Propaganistas/Laravel-Phone
-        $this->app->register(\Propaganistas\LaravelPhone\LaravelPhoneServiceProvider::class);
+        $this->app->register(LaravelPhoneServiceProvider::class);
 
         // laravel multilingual package
         // https://github.com/mcamara/laravel-localization
-        $this->app->register(\Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class);
+        $this->app->register(LaravelLocalizationServiceProvider::class);
 
         // laravel image validator
         // https://github.com/cviebrock/image-validator
-        $this->app->register(\Cviebrock\ImageValidator\ImageValidatorServiceProvider::class);
+        $this->app->register(ImageValidatorServiceProvider::class);
 
         // laravel breadcrumb
         // http://laravel-breadcrumbs.davejamesmiller.com/en/latest/start.html
-        $this->app->register(\DaveJamesMiller\Breadcrumbs\ServiceProvider::class);
+        $this->app->register(BreadcrumbsServiceProvider::class);
 
         // laravel model translatable
         // https://github.com/dimsav/laravel-translatable
-        $this->app->register(\Dimsav\Translatable\TranslatableServiceProvider::class);
+        $this->app->register(TranslatableServiceProvider::class);
     }
 }
