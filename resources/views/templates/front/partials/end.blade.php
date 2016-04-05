4a522734331008b906cf2df9f4d3eb0bcada6904
Arthur LORENT <artu1000>
2016-04-06 01:56:16 +0200

    google analytics code front implementation from settings

--- resources/views/templates/front/partials/end.blade.php
+++ resources/views/templates/front/partials/end.blade.php
@@ -1,2 +1,8 @@
 {{-- minified & versionned js file --}}
-<script async="async" type="text/javascript" src="{{ $js or url(elixir('js/app.front.js')) }}"></script>
+<script type="text/javascript" src="{{ $js or url(elixir('js/app.front.js')) }}" async defer></script>
+
+{{-- google analytics code insertion --}}
+@if($ga_code = config('settings.google_analytics'))
+    <script async defer>{{ $ga_code }}</script>
+@endif
+
