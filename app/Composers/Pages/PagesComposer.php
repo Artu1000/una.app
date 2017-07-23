51590cfe1df7addf2792f511cdab24804e2ffea2
Mickael Tardy <mickael.tardy@keosys.com>
2017-07-23 17:32:15 +0200

    List pages

--- app/Composers/Pages/PagesComposer.php
+++ app/Composers/Pages/PagesComposer.php
@@ -41,5 +41,60 @@ class PagesComposer
             ->where('slug', 'mentions-legales')
             ->where('active', true)
             ->first();
+        $view->juniors = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'juniors')
+            ->where('active', true)
+            ->first();
+        $view->university = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'aviron-universitaire')
+            ->where('active', true)
+            ->first();
+        $view->masters = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'aviron-loisirs-masters')
+            ->where('active', true)
+            ->first();
+        $view->openday = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'portes-ouvertes')
+            ->where('active', true)
+            ->first();
+        $view->summercamp = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'stage-d-ete')
+            ->where('active', true)
+            ->first();
+        $view->trainingcamp = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'stage-club')
+            ->where('active', true)
+            ->first();
+        $view->indoor_rowing = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'aviron-indoor')
+            ->where('active', true)
+            ->first();
+        $view->crossfit = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'aviron-crossfit')
+            ->where('active', true)
+            ->first();
+        $view->schools = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'scolaires')
+            ->where('active', true)
+            ->first();
+        $view->corporate = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'aviron-entreprise')
+            ->where('active', true)
+            ->first();
+        $view->custom = app(PageRepositoryInterface::class)
+            ->getModel()
+            ->where('slug', 'aviron-a-la-demande')
+            ->where('active', true)
+            ->first();
     }
 }
