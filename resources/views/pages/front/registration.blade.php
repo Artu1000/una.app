cec8416b8dba98405d42d7451a0ccc62e51dd2bc
Mickael Tardy <mickael.a.tardy@gmail.com>
2017-09-03 19:46:59 +0200

    Update registration page
    - New link for students
    - General conditions to be printed

--- resources/views/pages/front/registration.blade.php
+++ resources/views/pages/front/registration.blade.php
@@ -21,9 +21,8 @@
                 
                 <h3>Pour vous inscrire au club vous avez plusieurs options : </h3> 
                 <ul>
-                    <li>Pour les étudiants de l'Université de Nantes et le personnel de l'Université de Nantes vous devez passer par l'inscription au SUAPS (plus d'info <a href="http://www.univ-nantes.fr/les-activites-du-suaps/aviron-115148.kjsp?RH=1184590345081" target="_blank">ici</a>)</li>
+                    <li>Pour les étudiants de l'Université de Nantes et le personnel de l'Université de Nantes vous devez passer par l'inscription au SUAPS (plus d'info <a href="http://www.univ-nantes.fr/les-activites-du-suaps/aviron-115148.kjsp?RH=1184590345081" target="_blank">ici</a>) Vous devez passer par <a href="<a href="https://www.helloasso.com/associations/universite-de-nantes-aviron/adhesions/inscriptions-etudiantes" target="_blank">">l'inscription en ligne UNA</a> avant de vous rendre au SUAPS.</li>
                     <li>Pour tous les autres, vous pouvez-vous inscrire en ligne via le <a href="https://www.helloasso.com/associations/universite-de-nantes-aviron/adhesions/adhesions-saison-2017-2018" target="_blank">formulaire de Hello Asso</a></li>
-                    <li style="color:#aaa">Pour les universitaires, vous pouvez aussi passer par <a href="https://www.helloasso.com/associations/universite-de-nantes-aviron/adhesions/adhesions-saison-2017-2018" target="_blank">Hello Asso</a> pour nous simplifier le travail de collecte de données et de vos certificats médicaux</li>
                 </ul>
 
                 <h3>Plusieurs moyens de paiement sont disponibles </h3>
@@ -38,6 +37,11 @@
                     <li>Si vous avez fourni un certificat il y a moins d'un an, il est désormais valable 3 saisons.</li>
                     <li>En revanche, vous devez répondre au questionnaire <a href="https://www.formulaires.modernisation.gouv.fr/gf/cerfa_15699.do" target="">cerfa en ligne</li>
                     <li>Et joindre <a href="http://univ-nantes-aviron.fr/libraries/files/universite-nantes-aviron-una-file-5481261731.docx">l'attestation</a> signée lors de l'inscription.</li>
+                </ul>
+                
+                <h3>Décharge de responsabilité</h3>
+                <ul>
+                    <li>Veuillez télécharger et signer <a href="http://univ-nantes-aviron.fr/libraries/files/universite-nantes-aviron-una-file-9672610108.pdf" target="_blank">la décharge de responsabilité</a>, et ensuite la joindre au formulaire</li>
                 </ul>
                 
                 <h3>Des reductions sont proposées pour le tarif Tout public</h3>
