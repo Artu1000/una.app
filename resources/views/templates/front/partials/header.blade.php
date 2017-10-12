ef506908559111869f300f09309cd8c05fe6f2ca
Mickael Tardy <mickael.a.tardy@gmail.com>
2017-10-12 12:18:49 +0200

    Fix syntax typo

--- resources/views/templates/front/partials/header.blade.php
+++ resources/views/templates/front/partials/header.blade.php
@@ -74,8 +74,8 @@
                                     {{ trans('template.front.header.calendar') }}
                                 </a>
                             </li>
-                            <li class="menu_tab @if(Request::path() === route('page.show', ['slug' => practical_info->slug])) active @endif">
-                                <a href="{{ route('page.show', ['slug' => practical_info->slug]) }}" title="{{ trans('template.front.header.practical_info') }}">
+                            <li class="menu_tab @if(Request::path() === route('page.show', ['slug' => $practical_info->slug])) active @endif">
+                                <a href="{{ route('page.show', ['slug' => $practical_info->slug]) }}" title="{{ trans('template.front.header.practical_info') }}">
                                     {{ trans('template.front.header.practical_info') }}
                                 </a>
                             </li>
